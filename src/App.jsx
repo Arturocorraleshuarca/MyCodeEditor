ee4986e9232d3acb2218aa94435f6c875d96465c
Arturocorraleshuarca <Arthur23411@gmail.com>
2024-04-04 17:32:33 -0500

    image change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 import BarraTareas from './components/BarraTareas'
 import EditorWindows from './components/EditorWindows'
